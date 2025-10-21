5ca5efd729ea5d0b2aa7185c57ba3b1546e3ab84
rahul22b <rahul2212005@gmail.com>
2025-10-22 00:51:49 +0530

    sameSite none added

--- controllers/userController.js
+++ controllers/userController.js
@@ -118,7 +118,7 @@ export const login = async (req, res, next) => {
   res.cookie("sid", sessionId, {
     httpOnly: true,
     signed: true,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: sessionExpiryTime,
   });
   res.json({ message: "logged in" });
