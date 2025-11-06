9c756d2140a0943ff28bf1da765a9a8ab55ce9e1
rahul22b <rahul2212005@gmail.com>
2025-11-06 14:24:37 +0530

    dummy get route defined

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(
   })
 );
 
+app.get("/",(req,res)=>{
+return res.status(200).json({"message":"welcome to storage App"})
+})
+
 app.use("/directory", checkAuth, directoryRoutes);
 app.use("/file", checkAuth, fileRoutes);
 app.use("/", userRoutes);
