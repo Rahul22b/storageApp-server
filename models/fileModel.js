b49aba84b1e6a6ad7624922ea05d6779b952be86
rahul22b <rahul2212005@gmail.com>
2025-12-01 11:46:43 +0530

    aa

--- models/fileModel.js
+++ models/fileModel.js
@@ -5,14 +5,26 @@ const fileSchema = new Schema(
     name: {
       type: String,
       required: true,
+      trim: true, // Remove whitespace from both ends
+      minlength: [1, 'File name cannot be empty.'],
     },
     size: {
       type: Number,
       required: true,
+      min: [0, 'File size cannot be negative.'] // Ensure size is not negative
     },
     extension: {
       type: String,
       required: true,
+      trim: true,
+      lowercase: true,
+      // Ensure extension starts with '.' and has length > 1
+      validate: {
+        validator: function(v) {
+          return v.length > 1 && v.startsWith('.');
+        },
+        message: props => `${props.value} is not a valid file extension format (e.g., .png).`
+      }
     },
     userId: {
       type: Schema.Types.ObjectId,
@@ -21,16 +33,29 @@ const fileSchema = new Schema(
     parentDirId: {
       type: Schema.Types.ObjectId,
       ref: "Directory",
+      default: null // Explicitly set default for clarity
     },
-     deletedAt: {
+    deletedAt: {
       type: Schema.Types.Date,
       default: null,
     },
-    gcsGeneration: { type: String, default: null }
+    gcsGeneration: { 
+      type: String, 
+      default: null,
+      validate: {
+        // Only allow gcsGeneration to be set if the file is marked as deleted
+        validator: function(v) {
+          if (v && this.deletedAt === null) {
+            return false;
+          }
+          return true;
+        },
+        message: 'gcsGeneration can only be set when deletedAt is not null.'
+      }
+    }
   },
-  
   {
-    strict: "throw",
+    strict: "throw", // Ensures Mongoose throws an error if an invalid field is passed
     timestamps: true,
   }
 );
